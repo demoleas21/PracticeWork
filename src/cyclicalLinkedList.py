aea9433530940422f65109794692f04a706da7a2
demoleas21 <andrew.demoleas@gmail.com>
2017-07-10 21:51:04 -0400

    File framework

---
+++ src/cyclicalLinkedList.py
@@ -0,0 +1,17 @@
+class LinkedListNode(object):
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def containsCycle(node):
+    pass
+
+
+def main():
+    containsCycle()
+
+
+if __name__ == '__main__':
+    main()
